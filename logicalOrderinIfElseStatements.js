4860101c801cebab8a23ac2a3ac31fc751c649fd
Ramaano <61529495+ramaanoFullStack@users.noreply.github.com>
2022-02-20 23:22:12 +0200

    CHALLENGE SOLUTION

--- logicalOrderinIfElseStatements.js
+++ logicalOrderinIfElseStatements.js
@@ -1,3 +1,5 @@
+// CHALLENEGE
+
 function orderMyLogic(val) {
   if (val > 10) {
     return "Less than 10";
@@ -9,3 +11,17 @@ function orderMyLogic(val) {
 }
 
 console.log(orderMyLogic(6));
+
+// ANSWER
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+console.log(orderMyLogic(4));
