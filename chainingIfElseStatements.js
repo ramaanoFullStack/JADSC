9e1486236d82701bfdfb58dd0133c925ab2be487
Ramaano <61529495+ramaanoFullStack@users.noreply.github.com>
2022-02-21 08:58:36 +0200

    Solution to Problem

--- chainingIfElseStatements.js
+++ chainingIfElseStatements.js
@@ -9,3 +9,23 @@ function testSize(num) {
 }
 
 testSize(7);
+
+// ANSWER
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5){
+  return "Tiny";
+} else if (num < 10){
+  return "Small";
+} else if (num < 15){
+  return "Medium";
+} else if (num < 20){
+  return "Large";
+} else if (num >= 20){
+  return "Huge";
+}
+  // Only change code above this line
+}
+
+testSize(7);
