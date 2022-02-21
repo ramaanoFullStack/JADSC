ce172f333728e0931c4465ada68eaed46f444620
Ramaano <61529495+ramaanoFullStack@users.noreply.github.com>
2022-02-21 10:46:28 +0200

    Default Case
    Switch Statements  and the default option  challenge and solution

--- switchStatements.js
+++ switchStatements.js
@@ -36,3 +36,43 @@ switch (val){
 }
 
 caseInSwitch(1);
+
+// Default case Challenge
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+// Default Case Solution
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val){
+  case "a":
+    return "apple";
+    break;
+  case "b":
+    return "bird";
+    break;
+  case "c":
+    return "cat";
+    break;
+  default:
+    return "stuff";
+    break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
