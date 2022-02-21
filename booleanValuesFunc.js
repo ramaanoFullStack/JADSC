e72fab0ca7192851c6a7b077fbf092a450eef6df
Ramaano <rami.stalin.m@outlook.com>
2022-02-21 12:19:06 +0200

    Returning statement early Pattern for functions

--- booleanValuesFunc.js
+++ booleanValuesFunc.js
@@ -23,3 +23,35 @@ function isLess(a, b) {
 }
 
 isLess(10, 15);
+
+
+//Returning Early Pattern for Function
+
+//Challenge
+
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+
+
+    // Only change code above this line
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+
+//Solution
+
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+    if (a, b < 0){
+        return undefined;
+    }
+    // Only change code above this line
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+console.log(abTest(-2,-2));
