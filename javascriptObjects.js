7914933b9ced9fd4504dd1d8e7b751255ebc9ed8
Ramaano <rami.stalin.m@outlook.com>
2022-02-22 22:10:11 +0200

    Update the myDog object's name property. Let's change her name from Coder to Happy Coder. You can use either dot or bracket notation.

--- javascriptObjects.js
+++ javascriptObjects.js
@@ -125,3 +125,32 @@ const testObj = {
 const playerNumber = 16;  // Change this line
 const player = testObj[playerNumber];   // Change this line
 
+//Updating Object Properties
+
+//Challenge
+
+/*
+Update the myDog object's name property. Let's change her name from Coder to Happy Coder.
+You can use either dot or bracket notation.
+*/
+
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+// Only change code below this line
+
+//Solution
+
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+// Only change code below this line
+myDog.name = "Happy Coder";
