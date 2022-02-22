5b1ff00d4a6d153014945e8342e177d4c5fd68b2
Ramaano <rami.stalin.m@outlook.com>
2022-02-22 22:34:21 +0200

    Delete the tails property from myDog. You may use either dot or bracket notation.

--- javascriptObjects.js
+++ javascriptObjects.js
@@ -181,3 +181,36 @@ const myDog = {
 };
 
 myDog["bark"] = "woof";
+
+// Delete Properties from a JavaScript Object
+
+//Challenge
+
+/*
+Delete the tails property from myDog. You may use either dot or bracket notation.
+*/
+
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+
+// Only change code below this line
+
+//Solution
+
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails;
