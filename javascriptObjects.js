58e5451f3a4fb3d7a2aaf9f50f87f34b28eaee68
Ramaano <rami.stalin.m@outlook.com>
2022-02-22 21:34:48 +0200

    Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property     hat and set the variable shirtValue equal to the object's property shirt.

--- javascriptObjects.js
+++ javascriptObjects.js
@@ -25,3 +25,36 @@ const myDog = {
 
     // Only change code above this line
 };
+
+//Accessing Objects Properties with Dot Notation
+
+//Challenge
+
+/*
+    Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property
+    hat and set the variable shirtValue equal to the object's property shirt.
+*/
+
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj;      // Change this line
+const shirtValue = testObj;    // Change this line
+
+//Solution
+
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
