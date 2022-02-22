c73a529afa8af6d3ec3c1ec559a53dfb388e370a
Ramaano <rami.stalin.m@outlook.com>
2022-02-22 21:25:37 +0200

    An object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.     You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers,     and friends is an array.

---
+++ javascriptObjects.js
@@ -0,0 +1,27 @@
+// Building JavaScript Objects
+
+//Challenge
+
+/*
+    Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
+    You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers,
+    and friends is an array.
+*/
+
+const myDog = {
+    // Only change code below this line
+
+    // Only change code above this line
+};
+
+//Solution
+
+const myDog = {
+    // Only change code below this line
+    "name": "Tamtam",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["FriendOne","FriendTwo"]
+
+    // Only change code above this line
+};
