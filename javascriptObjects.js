0f78c8ca344d2f1816504264c7d4061098a0c433
Ramaano <rami.stalin.m@outlook.com>
2022-02-22 23:18:58 +0200

    Modify the function checkObj to test if an object passed to the function (obj) contains a specific property (checkProp). If the property is found, return that property's value. If not, return "Not Found".

--- javascriptObjects.js
+++ javascriptObjects.js
@@ -276,3 +276,32 @@ function phoneticLookup(val) {
 }
 
 phoneticLookup("charlie");
+
+//Testing Objects for Properties
+
+//Challenge
+
+/*
+Modify the function checkObj to test if an object passed to the function (obj) contains a specific property (checkProp).
+If the property is found, return that property's value. If not, return "Not Found".
+*/
+
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+        return "Change Me!"
+    // Only change code above this line
+}
+
+//Solution
+
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+    if(obj.hasOwnProperty(checkProp)){
+        return obj[checkProp];
+    } else if(obj.hasOwnProperty(checkProp)!==true){
+        return "Not Found"
+    } else {
+        return "Change Me!"
+    }
+    // Only change code above this line
+}
