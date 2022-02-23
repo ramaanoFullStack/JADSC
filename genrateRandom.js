ade33bcf38e746232357cd51c1467346ce01c70e
Ramaano <rami.stalin.m@outlook.com>
2022-02-23 17:34:03 +0200

    Change randomFraction to return a random number instead of returning 0.

---
+++ genrateRandom.js
@@ -0,0 +1,24 @@
+// Generate Random Fractions with Javascript
+
+// Challenge
+
+/*
+Change randomFraction to return a random number instead of returning 0.
+*/
+
+function randomFraction() {
+
+    // Only change code below this line
+
+    // Only change code above this line
+}
+
+// Solution
+function randomFraction() {
+
+    // Only change code below this line
+
+    return Math.random();
+
+    // Only change code above this line
+}
