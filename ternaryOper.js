7bc4acb34106f3ac1f515eac366aa68b8f25672d
Ramaano <rami.stalin.m@outlook.com>
2022-02-23 23:19:10 +0200

    In the checkSign function, use multiple conditional operators - following the recommended format used in findGreaterOrEqual - to check if a number is positive, negative or zero. The function should return positive, negative or zero.

--- ternaryOper.js
+++ ternaryOper.js
@@ -24,3 +24,28 @@ function checkEqual(a, b) {
 }
 
 checkEqual(1, 2);
+
+// Use Multiple Conditional (Ternary) Operators
+
+// Challenge
+
+/*
+In the checkSign function, use multiple conditional operators
+- following the recommended format used in findGreaterOrEqual
+- to check if a number is positive, negative or zero.
+The function should return positive, negative or zero.
+*/
+
+function checkSign(num) {
+
+}
+
+checkSign(10);
+
+// Solution
+
+function checkSign(num) {
+    return (num > 0) ? "positive": (num < 0)? "negative": "zero";
+}
+
+console.log(checkSign(10));
