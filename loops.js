575ff4acde56a35a5e51c3a7e7dc22fcc7391448
Ramaano <rami.stalin.m@outlook.com>
2022-02-23 14:18:06 +0200

    Use a for loop to push the values 1 through 5 onto myArray.

--- loops.js
+++ loops.js
@@ -23,3 +23,26 @@ while (i > -1) {
     myArray.push(i);
     i--;
 }
+
+//Iterate with Javascript For loops
+
+//Challenge
+
+/*
+Use a for loop to push the values 1 through 5 onto myArray.
+*/
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+
+//Solution
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 6; i++){
+    myArray.push(i);
+}
