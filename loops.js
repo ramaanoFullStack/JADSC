1eea91fb1b1296aa56e64b73921d473c994b7151
Ramaano <rami.stalin.m@outlook.com>
2022-02-23 15:38:04 +0200

    Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, and i will be equal to 11 when your code has finished running.

--- loops.js
+++ loops.js
@@ -151,3 +151,35 @@ function multiplyAll(arr) {
 }
 
 console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]));
+
+// Iterate with JavaScript Do..While Loops
+
+//Challenge
+
+/*
+Change the while loop in the code to a do...while loop so the loop will push
+only the number 10 to myArray, and i will be equal to 11 when your code has
+finished running.
+*/
+
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+while (i < 5) {
+    myArray.push(i);
+    i++;
+}
+
+// Solution
+
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+    myArray.push(i);
+    i++;
+} while (i < 5);
