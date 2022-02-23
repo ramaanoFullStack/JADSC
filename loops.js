9f196a526cc968f8acc2924f508bad1ffe229c1e
Ramaano <rami.stalin.m@outlook.com>
2022-02-23 16:39:59 +0200

    Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.

--- loops.js
+++ loops.js
@@ -183,3 +183,34 @@ do {
     myArray.push(i);
     i++;
 } while (i < 5);
+
+// Replacing Loops using Recursion
+
+/*
+What is Recursion?
+Recursion is the concept that a function can be expressed in terms of itself.
+*/
+
+/*
+Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+*/
+
+// Challenge
+
+function sum(arr, n) {
+    // Only change code below this line
+
+    // Only change code above this line
+}
+
+// Solution
+
+function sum(arr, n) {
+    // Only change code below this line
+    if (n<=0){
+        return 0;
+    } else {
+        return sum(arr,n -1) + arr[n-1];
+    }
+    // Only change code above this line
+}
