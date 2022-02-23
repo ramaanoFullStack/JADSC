46049ac11168ebbeb26236193e68986417f17b61
Ramaano <rami.stalin.m@outlook.com>
2022-02-23 22:42:47 +0200

    Use parseInt() in the convertToInteger function, so it converts the input string str into an integer, and returns it.

---
+++ parseintFunc.js
@@ -0,0 +1,23 @@
+// Use the parseInt Function
+
+/*
+The parseInt() function parses a string and returns an integer. Here's an example:
+*/
+
+// Challenge
+
+/*
+Use parseInt() in the convertToInteger function, so it converts the input string str into an integer, and returns it.
+*/
+
+function convertToInteger(str) {
+
+}
+
+// Solution
+
+function convertToInteger(str) {
+    return parseInt(str);
+}
+
+console.log(convertToInteger("007"));
