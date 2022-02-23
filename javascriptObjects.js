eec404044bc80241b80361f0083e93c9b0b8df42
Ramaano <rami.stalin.m@outlook.com>
2022-02-23 09:15:30 +0200

    Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable. Use dot notation for all properties where possible, otherwise use bracket notation.

--- javascriptObjects.js
+++ javascriptObjects.js
@@ -354,3 +354,42 @@ const myMusic = [
         ]
     }
 ];
+
+// Accessing Nested Objects
+
+//Challenge
+
+/*
+Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable.
+Use dot notation for all properties where possible, otherwise use bracket notation.
+*/
+
+const myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+};
+
+const gloveBoxContents = undefined;
+
+//Solution
+
+const myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
