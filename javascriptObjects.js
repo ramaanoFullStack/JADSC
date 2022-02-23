d69e5a2f8aeaa7354143e07d93b73e0440870aad
Ramaano <rami.stalin.m@outlook.com>
2022-02-23 09:46:44 +0200

    Using dot and bracket notation, set the variable secondTree to the second item in the trees list from the myPlants object.

--- javascriptObjects.js
+++ javascriptObjects.js
@@ -393,3 +393,56 @@ const myStorage = {
 };
 
 const gloveBoxContents = myStorage.car.inside["glove box"];
+
+// Accessing Nested Arrays
+
+// Challenge
+
+/*
+Using dot and bracket notation, set the variable secondTree to
+the second item in the trees list from the myPlants object.
+*/
+
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+const secondTree = "";
+
+// Solution
+
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+const secondTree = myPlants[1].list[1];
