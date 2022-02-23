8ec2629c25012099cf716ff622b211dd339c3754
Ramaano <rami.stalin.m@outlook.com>
2022-02-23 14:05:23 +0200

    Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.

---
+++ loops.js
@@ -0,0 +1,25 @@
+//Iterate with Javascript While loops
+
+//Challenge
+
+/*
+Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+*/
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+
+//Solution
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 5
+
+while (i > -1) {
+    myArray.push(i);
+    i--;
+}
