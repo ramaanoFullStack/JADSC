6fee0765024af73799f520c01859daa70de7a384
Ramaano <rami.stalin.m@outlook.com>
2022-02-26 12:52:06 +0200

    Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables highToday and highTomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.

--- destructuringAssign.js
+++ destructuringAssign.js
@@ -37,3 +37,40 @@ const HIGH_TEMPERATURES = {
 const {today, tomorrow} = HIGH_TEMPERATURES;
 
 // Only change code above this line
+
+// Use Destructuring Assignment to Assign Variables from Objects
+
+// Challenge
+
+/*
+Replace the two assignments with an equivalent destructuring assignment.
+It should still assign the variables highToday and highTomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+*/
+
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+
+const highToday = HIGH_TEMPERATURES.today;
+const highTomorrow = HIGH_TEMPERATURES.tomorrow;
+
+// Only change code above this line
+
+// Solution
+
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+
+const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
+
+// Only change code above this line
+
