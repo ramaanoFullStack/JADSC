7b06a9eb3ae9fb289a4c10a59173c4c338a0cc04
Ramaano <rami.stalin.m@outlook.com>
2022-02-26 12:33:18 +0200

    Modify the function sum using the rest parameter in such a way that the function sum is able to take any number of arguments and return their sum.

--- arrowFunc.js
+++ arrowFunc.js
@@ -52,4 +52,25 @@ const increment = (number, value) => number + value;
 
 const increment = (number, value = 1) => number + value;
 
+// Use the Rest Parameter with Function Parameters
+
+// Challenge
+
+/*
+Modify the function sum using the rest parameter in such a way that the function sum is able to take any number of arguments and return their sum.
+*/
+
+const sum = (x, y, z) => {
+    const args = [x, y, z];
+    return args.reduce((a, b) => a + b, 0);
+}
+
+// Solution
+
+const sum = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
+}
+console.log(sum(5))
+
+
 
